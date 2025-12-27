915de41f61d088b7fa9c8115077aaff83d50ad50
Deebyanshu <jhadeebyanshu@gmail.com>
2025-12-27 14:09:33 +0530

    js-part1-compt

---
+++ javascript/first.js
@@ -0,0 +1,29 @@
+console.log("hello world!");
+console.table({city: "jaipur"});
+
+/* data types: 
+    String
+    Number
+    Boolean
+    BigInt
+
+    Undefined
+    null
+
+    object
+
+    symbol
+*/
+
+var score = 102
+let name = "deebyanshu"
+let isLoggedIn = true
+
+let getScore = score
+score = 105
+console.log(getScore);
+
+const result = 91.5
+
+console.log(result);
+
